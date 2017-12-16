f632203df8f7b8a3e36257c07b4e03317a31881e
mefedron <pikusamichal3000@gmail.com>
2017-12-16 13:25:56 +0100

    atom
    zmiana właściciela configu vim instalacja atom

--- skrypt.sh
+++ skrypt.sh
@@ -1,18 +1,28 @@
-#skrypt uruchamiac tylko po przez pakiet sudo oraz znajadujac sie w katalogu ze skryptem
+#skrypt uruchamiać tylko po przez pakiet sudo oraz znajadując sie w katalogu ze skryptem
 
-	#odswiezenie listy pakietow
+	#odswieżenie listy pakietów
 apt-get update
 
 	#instalacja wybranych pakietow
-	#paiety do instalacji odzielac spacja
+	#paiety do instalacji odzielać spacją
 apt-get install -y vim tree nmon tmux gparted git
 
-	#kopiowanie i zmiana uprawnien pliku konfiguracyjnego vim
+	#kopiowanie i zmiana uprawnień, grupy, oraz właściciela pliku konfiguracyjnego vim'a
 cp ./.vimrc ~/
 chmod 664 ~/.vimrc
+chown egzamin ~/.vimrc
+chgrp egzamin ~/.vimrc
+
+	#aktualizacja wszystkich pakietów
+apt-get upgrade -y
+
+	#pobranie edytora atom
+wget https://github.com/atom/atom/releases/download/v1.23.1/atom-amd64.deb
+	#instalacja edytora atom
+dpkg --install atom-amd64.deb
 
 
 
-#Wersja: 1.0
+#Wersja: 1.1
 #Autor: Michał Pikusa
 #Licencja: MIT
