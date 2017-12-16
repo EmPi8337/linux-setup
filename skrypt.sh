2209b05c160252bf7b4d11475fc0201b7656fb19
mefedron <pikusamichal3000@gmail.com>
2017-12-16 13:57:34 +0100

    Czyszczenie pulpitu

--- skrypt.sh
+++ skrypt.sh
@@ -1,28 +1,47 @@
-#skrypt uruchamiać tylko po przez pakiet sudo oraz znajadując sie w katalogu ze skryptem
+#Skrypt uruchamiać tylko po przez pakiet sudo oraz znajadując sie w katalogu ze skryptem
 
 	#odswieżenie listy pakietów
-apt-get update
-
-	#instalacja wybranych pakietow
-	#paiety do instalacji odzielać spacją
-apt-get install -y vim tree nmon tmux gparted git
-
-	#kopiowanie i zmiana uprawnień, grupy, oraz właściciela pliku konfiguracyjnego vim'a
-cp ./.vimrc ~/
-chmod 664 ~/.vimrc
-chown egzamin ~/.vimrc
-chgrp egzamin ~/.vimrc
-
-	#aktualizacja wszystkich pakietów
-apt-get upgrade -y
-
-	#pobranie edytora atom
-wget https://github.com/atom/atom/releases/download/v1.23.1/atom-amd64.deb
-	#instalacja edytora atom
-dpkg --install atom-amd64.deb
+	apt-get update
 
 
+	#Instalacja wybranych pakietów
+		#Pakiety do instalacji odzielać spacją
+	apt-get install -y vim tree nmon tmux gparted git
 
-#Wersja: 1.1
+
+#Kopia i zmiana uprawnień, grupy, oraz właściciela pliku konfiguracyjnego vim'a
+
+	#Kopia
+	cp ./.vimrc ~/
+	#Zmiana uprawnień
+	chmod 664 ~/.vimrc
+	#Zmiana właściciela
+	chown egzamin ~/.vimrc
+	#Zmiana grupy
+	chgrp egzamin ~/.vimrc
+
+
+#Aktualizacja wszystkich pakietów
+	apt-get upgrade -y
+
+
+#Pobranie edytora atom oraz instalacja
+
+	#Pobranie
+	wget https://github.com/atom/atom/releases/download/v1.23.1/atom-amd64.deb
+	#Instalacja
+	dpkg --install atom-amd64.deb
+
+
+#Usunięcie początkowych plików i katalogów na pulpicie
+
+	#usunięcie katalogów
+	rm -r ~/Pulpit/*
+	#usunięcie plików
+	rm ~/Pulpit/*
+
+
+
+#Wersja: 1.2
 #Autor: Michał Pikusa
 #Licencja: MIT
